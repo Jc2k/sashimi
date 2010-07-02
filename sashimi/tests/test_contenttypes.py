63f77ef9f9eaa516696b6959ba622950a05e7560
John Carr <john.carr@isotoma.com>
2010-07-02 23:05:08 +0100

    Test having a content type to iterate

--- sashimi/tests/test_contenttypes.py
+++ sashimi/tests/test_contenttypes.py
@@ -3,6 +3,30 @@ from mock import Mock
 
 from sashimi.contenttypes import ContentTypeVisitor, ContentTypeRoot
 
+
+class IndexableObject(object):
+    '''Creates an object that wraps around the mock object so when it is
+    indexable: mock_object[id] is the same as getattr(mock_object[id]) and
+    similarly with set'''
+
+    def __init__(self, mock_object):
+        # self._inner = mock_object means mock_object._inner = mock_object
+        # which causes a recursive call when we do a getattr for _inner
+        object.__setattr__(self, '_inner', mock_object)
+
+    def __getattr__(self, attr):
+        return getattr(self._inner, str(attr))
+
+    def __getitem__(self, item):
+        return self.__getattr__(item)
+
+    def __setattr__(self, attr, value):
+        setattr(self._inner, attr, value)
+
+    def __setitem__(self, item, value):
+        self.__setattr__(item, value)
+
+
 class TestContentTypes(unittest.TestCase):
 
     def test_no_content_types(self):
@@ -12,4 +36,11 @@ class TestContentTypes(unittest.TestCase):
         retval = c.visit_types()
         self.failUnless(isinstance(retval, ContentTypeRoot))
 
+    def test_simple_content_type(self):
+        portal = Mock()
+        portal.portal_types = IndexableObject(Mock())
+        portal.portal_types['Folder'].allowed_content_types = ['Folder']
+        portal.portal_types.listContentTypes.return_value = ["Folder"]
+        c = ContentTypeVisitor(portal)
+        retval = c.visit_types()
 
