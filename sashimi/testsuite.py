f43ee3430e8ae35e0036e369e2d6eab09f33f382
John Carr <john.carr@isotoma.com>
2010-07-03 22:52:13 +0100

    Combine the mixing FunctionalTestCase without our test

--- sashimi/testsuite.py
+++ sashimi/testsuite.py
@@ -1,9 +1,35 @@
 
 import unittest
 
+from AccessControl.SecurityManagement import newSecurityManager
+from Testing.makerequest import makerequest
+import transaction
+
+from sashimi.contenttypes import ContentTypeVisitor
+from sashimi.content import ContentMapVisitor
+
+
+class TestCase(object):
+
+    def login(app, manager_user):
+        user = app.acl_users.getUserById(manager_user).__of__(app.acl_users)
+        newSecurityManager(None, user)
+        return makerequest(app)
+
+    def test_fuzz(self):
+        #app = login(app, "zopeadmin")
+
+        a = ContentTypeVisitor(self.portal)
+        map = a.visit_types()
+
+        b = ContentMapVisitor(map)
+        b.fuzz()
+
+
 class TestSuite(unittest.TestSuite):
 
     def __init__(self, mixin):
-        self.mixin = mixin
-
+        super(TestSuite, self).__init__()
+        cls = type('TestFuzzing', (mixin, TestCase), {})
+        self.addTest(unittest.makeSuite(cls))
 
