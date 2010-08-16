2bb93d0e3369d8fdf80c65a1cd77d02dd56ef67e
John Carr <john.carr@isotoma.com>
2010-08-17 00:40:04 +0100

    Generic file stuff

--- sashimi/generators/file.py
+++ sashimi/generators/file.py
@@ -6,6 +6,17 @@ def sibpath(path):
     d, f = os.path.split(__file__)
     return os.path.join(d, path)
 
+class File(object):
+
+    @classmethod
+    def can_fuzz(cls, field):
+        if field["type"] == "file":
+            return True
+        return False
+
+    def fuzz(self, field):
+        return open(sibpath("assets/test.mp3")).read()
+
 class Image(object):
 
     @classmethod
