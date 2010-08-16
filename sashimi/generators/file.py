c5bb9249b8dddcde9eaf27bd2782fad4dbfb6783
John Carr <john.carr@isotoma.com>
2010-08-17 00:18:33 +0100

    Add assets and a generator for file data

---
+++ sashimi/generators/file.py
@@ -0,0 +1,21 @@
+
+import random
+from sashimi.generators.registry import registry
+
+def sibpath(path):
+    d, f = os.path.split(__file__)
+    return os.path.join(d, path)
+
+class Image(object):
+
+    @classmethod
+    def can_fuzz(cls, field):
+        if field["type"] == "image":
+            return True
+        return False
+
+    def fuzz(self, field):
+        return open(sibpath("assets/test.png")).read()
+
+registry.register(Image)
+
